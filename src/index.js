d545d8943eef9991b9604777d45c88ef099b735c
ank-it07 <kumarankit908011@gmail.com>
2024-04-16 20:13:43 +0530

    utility code

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8001, () => {
+    app.listen(process.env.PORT || 8002, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
