849c58bdd18ecb60f3d90c42ca9c4f8c5d5831bd
ank-it07 <kumarankit908011@gmail.com>
2024-04-17 13:22:01 +0530

    all setup config

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes imports
+import userRouter from './routes/user.routes.js'
+
+// routes decelartion
+app.use("/api/v1/users", userRouter)
+
 
 
 export { app };
